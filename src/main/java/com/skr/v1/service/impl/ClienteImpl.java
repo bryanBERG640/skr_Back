390128296622d30581dca56dbdd6180f8706a054
bryanBERG640 <bryanberg640@gmail.com>
2019-12-26 13:48:06 -0600

    actualizacion

--- src/main/java/com/skr/v1/service/impl/ClienteImpl.java
+++ src/main/java/com/skr/v1/service/impl/ClienteImpl.java
@@ -3,7 +3,6 @@ package com.skr.v1.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import com.skr.v1.entity.Cliente;
 import com.skr.v1.repository.RepositoryCliente;
 import com.skr.v1.service.ClienteService;
